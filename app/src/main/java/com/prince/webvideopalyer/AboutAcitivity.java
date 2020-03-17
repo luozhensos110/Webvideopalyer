887e9ce54f0cd3b2b1d182e4d60954e834e012f5
luozhensos110 <you@example.com>
2020-03-17 11:29:39 +0800

    V1.3.2 修改About页面版本提示,实现自动获取版本信息
    Signed-off-by: luozhensos110 <you@example.com>

--- app/src/main/java/com/prince/webvideopalyer/AboutAcitivity.java
+++ app/src/main/java/com/prince/webvideopalyer/AboutAcitivity.java
@@ -1,14 +1,19 @@
 package com.prince.webvideopalyer;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class AboutAcitivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_acitivity);
+        textView = findViewById(R.id.textView_version);
+        String versionName = APKVersionCodeUtils.getVerName(this);
+        textView.setText("当前软件版本为"+versionName);
+
     }
 }
